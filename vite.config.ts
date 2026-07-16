6c5aee76a63aff4b344dfcfba4b97201c8077fd9
Anna Yao <annayao0602@gmail.com>
2026-07-16 17:09:59 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -34,3 +34,8 @@ export default defineConfig({
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
+
+export default defineConfig({
+  base: '/Agent_Tuning/', // Replace with your exact GitHub repo name
+  plugins: [react()],
+})
